7ad767d21c6ac86685296c9ad3dbffee1d97e0f7
chaopeng <139025826@qq.com>
2013-01-08 12:29:37 +0800

    fixed navibar wrong order

--- builder/src/pages/navibar/Navibar.java
+++ builder/src/pages/navibar/Navibar.java
@@ -1,6 +1,8 @@
 package pages.navibar;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
 
 import model.Article;
 
@@ -16,12 +18,19 @@ public class Navibar {
 		File[] files = DirUtils.ls(BlogConfig.getIns().inputpath+"/navibar");
 		NavibarLs = new Article[files.length];
 		
+		Arrays.sort(files, new Comparator<File>() {
+
+			@Override
+			public int compare(File o1, File o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+		});
+		
 		int i = -1;
 		for (File file : files) {
 			Article article = new Article();
 			article.readArticle(file.getAbsolutePath());
 			NavibarLs[++i] = article;
-			
 		}
 	}
 	public static Article[] getNavibarLs(){
