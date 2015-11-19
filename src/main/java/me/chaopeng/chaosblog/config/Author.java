dd5919f6aca95a7f871110d241db9d403ca7f0a8
chaopeng <139025826@qq.com>
2015-11-19 15:28:42 -0500

    add wechat settings

--- src/main/java/me/chaopeng/chaosblog/config/Author.java
+++ src/main/java/me/chaopeng/chaosblog/config/Author.java
@@ -14,49 +14,55 @@ public class Author {
     private String gravatarserver;
     private String weibo;
     private String github;
+    private String wechat;
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public void setGravatar(String gravatar) {
-        this.gravatar = gravatar;
-    }
-
-    public void setGravatarserver(String gravatarserver) {
-        this.gravatarserver = gravatarserver;
-    }
-
-    public void setWeibo(String weibo) {
-        this.weibo = weibo;
-    }
-
-    public void setGithub(String github) {
-        this.github = github;
-    }
-
-    public String getAvatar() {
-        return GravatarUtils.getAvatar(gravatarserver, gravatar, "80");
-    }
 
     public String getMail() {
         return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
     }
 
     public String getGravatar() {
         return gravatar;
     }
 
+    public void setGravatar(String gravatar) {
+        this.gravatar = gravatar;
+    }
+
     public String getGravatarserver() {
         return gravatarserver;
+    }
+
+    public void setGravatarserver(String gravatarserver) {
+        this.gravatarserver = gravatarserver;
     }
 
     public String getWeibo() {
         return weibo;
     }
 
+    public void setWeibo(String weibo) {
+        this.weibo = weibo;
+    }
+
     public String getGithub() {
         return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat;
     }
 
     public static Author getIns() {
